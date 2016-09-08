7294b11f4ec0aebce2f463340a428dfb925b33ee
Tom Robertshaw <me@tomrobertshaw.net>
2016-09-08 21:32:51 +0100

    Refactor webserver out into library and remove from index.js

--- index.js
+++ index.js
@@ -2,23 +2,12 @@
 
 var http = require('http');
 var rugbybot = require('./lib/rugbybot');
+var webserver = require('./lib/webserver');
 
 var team = 'bath rugby';
 var bot = new rugbybot(team, true);
 bot.initialise();
 
-/* Listen to web requests in order to make sure
- * that bluemix doesn't shut the app down because it
- * thinks that it's crashed.
- */
-var server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('The Rugby Bot is listening for messages for ' + team);
-});
 
-server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-});
-
-var port = process.env.VCAP_APP_PORT || 8080;
-server.listen(port);
+var server = new webserver();
+server.start();
